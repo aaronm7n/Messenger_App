b985ee586dd4393302ce2b202674323ca1434b65
Joseph Johnson <johnsonj73@udayton.edu>
2023-11-26 19:42:12 -0500

    Add users to room works now! It was a silly mistake!

--- messengerApp/routes/add_to_room.js
+++ messengerApp/routes/add_to_room.js
@@ -13,15 +13,44 @@ const checkSignIn = (req, res, next) => {
     }
 };
 
-// Create Room GET Request
+
+// Add to room Room GET Request
 router.get('/', checkSignIn, (req, res) => {
     res.render('add_to_room');
 });
 
-// This allows us to know which user is currently logged in so we know what data to change
 router.use (function(req, res, next) {
     res.locals.currentUser = req.session.user.username;
     next();
 });
+
+
+router.post('/', async (req, res) => {
+    roomInfo = await req.body;
+    const room = await Room.findOne({ roomName: roomInfo.roomName, roomCode: roomInfo.roomCode })
+    console.log(roomInfo);
+
+    if (!roomInfo.roomName || !roomInfo.roomCode || !roomInfo.newUserName) {
+        return res.render('add_to_room', { message: "Please enter valid information to add a user!" });
+    }
+    else if (!room) {
+        return res.render('add_to_room', { message: "Room not found!" });
+    }
+    else {
+        const room = await Room.findOneAndUpdate(
+            { "roomName": roomInfo.roomName, "roomCode": roomInfo.roomCode },
+            { $push: { userList: roomInfo.newUserName } },
+            { new: true } // To return the updated document
+        );
+        console.log(room.userList);
+        await room.save();
+    
+        res.render('add_to_room', { message: "User Added Successfully!" });
+        
+        console.log("is anything happening at all bro");
+    }
+});
+
+// This allows us to know which user is currently logged in so we know what data to change
 
 module.exports = router;
